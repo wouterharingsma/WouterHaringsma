b1ec4a10780de71d1cc9a4569eefb6169cac606f
Wouter Haringsma <63634970+wouterharingsma@users.noreply.github.com>
2023-04-04 10:32:03 +0200

    Placeholder Init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
-<script>
-  import { base } from "$app/paths";
-</script>
-
-<header class="py-8 text-xs">
-  <div class="container mx-auto">
-    <div class="flex flex-row">
-      <div><h1>Wouter Haringsma</h1></div>
-      <div class="grow text-center">Articles</div>
-      <div class="flex justify-end" ><a href="{base}/portfolio">Portfolio</a></div>
+<div class="flex items-center justify-center h-screen text-sm text-slate-800">
+  <div class="flex flex-col text-center">
+    <div class="mb-2">
+      <h1 class="text-2xl font-bold">Wouter Haringsma</h1>
+    </div>
+    <div class="w-2/3 mx-auto mb-6">
+      <p>Human-centred Product Designer and Developer working on the future of data-driven and machine learning based user experiences.</p>
+    </div>
+    <div class="text-blue-800 text-base">
+      <ul>
+        <li><a href="https://www.linkedin.com/in/haringsmawouter/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"/></a></li>
+      </ul>
     </div>
   </div>
-</header>
+</div>
